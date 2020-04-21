cf6e9f4987c4de59d257d61a2adf6f1b86553aca
Dave Martin <david.martin@fundingcircle.com>
2020-04-21 11:58:01 +0100

    Try again to get automated build working

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-sbt fullOptJS::webpack && \
-cp target/scala-2.13/scalajs-bundler/main/jsonviewer-opt-bundle.js public/main.js && \
+if ! which sbt; then
+  curl -o sbt.zip https://sbt-downloads.cdnedge.bluemix.net/releases/v1.3.10/sbt-1.3.10.zip
+  unzip sbt.zip
+  export PATH="$PATH:$(pwd)/sbt/bin"
+fi
+
+
+sbt fullOptJS::webpack
+cp target/scala-2.13/scalajs-bundler/main/jsonviewer-opt-bundle.js public/main.js
 cp assets/main.css public/
