9ee1d6fff06d99e95149d2f9a5b5a3814d0f32b3
Dave Martin <david.martin@fundingcircle.com>
2020-04-21 12:17:44 +0100

    Build - download different sbt version

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! which sbt; then
-  curl -o sbt.zip https://sbt-downloads.cdnedge.bluemix.net/releases/v1.3.10/sbt-1.3.10.zip
+  curl -o sbt.zip https://sbt-downloads.cdnedge.bluemix.net/releases/v1.2.8/sbt-1.2.8.zip
   unzip sbt.zip
   export PATH="$PATH:$(pwd)/sbt/bin"
 fi
